c61e0e018602a31ea41ab88e11b8acdac1378ad2
Exaint <exaint@icloud.com>
2025-12-29 15:45:37 +0100

    Add shop link to first-time user message

--- _worker.js
+++ _worker.js
@@ -523,7 +523,7 @@ async function handleSlot(username, amountParam, url, env) {
     const selfBanData = await isSelfBanned(username, env);
     if (selfBanData) {
       const decoration = DECORATIONS[Math.floor(Math.random() * DECORATIONS.length)];
-      return new Response(`@${username} 🚫 Du hast dich selbst vom Spielen ausgeschlossen (seit ${selfBanData.date}). Kontaktiere einen Admin für eine Freischaltung. Hilfe: https://git.new/DachsbauSlotInfos ${decoration}`, { headers: RESPONSE_HEADERS });
+      return new Response(`@${username} 🚫 Du hast dich selbst vom Spielen ausgeschlossen (seit ${selfBanData.date}). Kontaktiere einen Admin für eine Freischaltung. Hilfe: https://git.new/DachsbauSlotInfos | Shop: https://git.new/DachsbauSlotsShop ${decoration}`, { headers: RESPONSE_HEADERS });
     }
     
     // First-Time Disclaimer Check
